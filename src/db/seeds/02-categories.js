8aa2961b6682ac2c070b3712da21c40910a27b47
yamodah <yassinedahlek@gmail.com>
2021-11-23 15:07:55 -0500

    added seeds info as well as adding .service files to connect quereis with database

--- src/db/seeds/02-categories.js
+++ src/db/seeds/02-categories.js
@@ -1,13 +1,9 @@
+const categories = require("../fixtures/categories");
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE categories RESTART IDENTITY CASCADE")
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex("categories").insert(categories);
     });
 };
--- src/db/seeds/03-products_categories.js
+++ src/db/seeds/03-products_categories.js
@@ -1,13 +1,9 @@
+const productsCategories = require("../fixtures/productsCategories");
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE products_categories RESTART IDENTITY CASCADE")
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex("products_categories").insert(productsCategories);
     });
 };
